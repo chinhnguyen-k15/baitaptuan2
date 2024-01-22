05c843255dfec6eb663ff77d3e32a748371e6815
chinhnguyen-k15 <157139763+chinhnguyen-k15@users.noreply.github.com>
2024-01-22 13:14:52 +0700

    Add files via upload

---
+++ MyThread.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaapplication2;
+
+
+public class MyThread extends Thread{
+
+    @Override
+    public void run() {
+        System.out.println("Luong dang chay");
+    }
+    public static void main(String[] args) {
+        MyThread th = new MyThread();
+        th.start();
+    }
+}
